fa885293989c5b628e1b4b7859a9a1541aeac57a
gabs-v <gabyvazquez8297@gmail.com>
2022-03-17 23:34:41 -0700

    first commit

---
+++ variable.py
@@ -0,0 +1,22 @@
+#Hello! :)I'm just doing some extra work on fine tuning and getting a little more comfortable with Python little by little! :)
+
+from operator import truediv
+
+
+name = "Gaby"
+my_age = "21"
+favorite_color_green = True
+favorite_color_red = False
+
+print ("Hello! my name is " + name + ", short for Gabriela.") 
+print ("Feel free to call me either of those. Sometimes people fancy one over the other.")
+print ("I'm currently " + my_age + " years old. ")
+print ("I used to really wanted to become a teacher while growing up but, as people do, I changed my mind")
+print ("It took me a bit to really realize what I want to do. I just kept on working at my local daycare while figuring what's is the next step.")
+print ("Then after looking a bit of time and pondering, I came to the decision that I want to presure software development.")
+print ("How fun.")
+print ("Out of two options, green or purple, what's my favories color?")
+favorite_color_green 
+favorite_color_red
+print ("Thats right! My favorite color is green!")
+print ("Now you know a bit about " + name + ".")
